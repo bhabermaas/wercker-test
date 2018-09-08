b037a6952d6d1d1389c27a8d70ef5fc21378b874
Bill Habermaas <bill.habermaas@oracle.com>
2018-09-08 13:56:56 -0400

    Another commit to test

--- hellotest.go
+++ hellotest.go
@@ -10,4 +10,5 @@ func main() {
 	fmt.Println("Hello universe")
 	fmt.Printf("That's a;ll folks")
 	fmt.Printf("Aaaarrggghhh!!")
+	fmt.Println("The quick brown fox jumped over the lazy dog's back")
 }
