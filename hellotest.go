d8f6b82da545c6c922e07c9dbda34bddde1838aa
Bill Habermaas <bill.habermaas@oracle.com>
2018-09-13 11:35:35 -0400

    Update test

--- hellotest.go
+++ hellotest.go
@@ -8,7 +8,7 @@ func main() {
 
 	fmt.Println("Hello world")
 	fmt.Println("Hello universe")
-	fmt.Printf("That's a;ll folks")
-	fmt.Printf("Aaaarrggghhh!!")
+	fmt.Println("That's all folks")
+	fmt.Println("Aaaarrggghhh!!")
 	fmt.Println("The quick brown fox jumped over the lazy dog's back")
 }
